92209bf3bee0e2e1a2fdc8ae88b5a23d0802d288
Luiz Henrique Simonato <151886300+LuiZordLord@users.noreply.github.com>
2024-07-02 14:10:04 -0300

    Update SomaNumeros.py

--- SomaNumeros.py
+++ SomaNumeros.py
@@ -3,3 +3,9 @@ numero2= input('Informe o segundo numero')
 soma= float(numero1) + float(numero2)
 print('A soma dos numeros é:', soma)
 
+//////////////
+
+numero1= int (input('Informe o primeiro numero'))
+numero2= int (input('Informe o segundo numero'))
+soma= numero1+numero2
+print('A soma dos numeros é:', soma)
