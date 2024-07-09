40ca617106bd39ee0cca3fcbc7b03c7f7dacfeca
Luiz Henrique Simonato <151886300+LuiZordLord@users.noreply.github.com>
2024-07-09 14:48:11 -0300

    Create ExStrings.py

---
+++ ExStrings.py
@@ -0,0 +1,13 @@
+frase = "Curso em Video Python"
+print(frase[0:21:3])
+print(len(frase))
+print(frase.upper())
+print(frase.find('o'))
+print(frase.capitalize())
+print(frase.title())
+print(frase.lower().find('video'))
+div = frase.split()
+print(div[2][3])
+
+#frase = frase.replace("Python", "Android") substituir texto
+#print(frase)
